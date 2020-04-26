63a4aea0188fd0086ee17d822b96b3f75b0a239e
cfujitani20 <cfujitani20@punahou.edu>
2020-04-26 09:04:11 +0000

    Fixed all bugs and final commit

--- Fraction.java
+++ Fraction.java
@@ -9,17 +9,21 @@ public class Fraction
         try{
             int quo = num / denom;
         }
-        catch(IllegalArguementException e){
-            System.out,println("Results in undefined, denominator cannot equal 0.");
+        catch(IllegalArgumentException e){
+            System.out.println("Results in undefined, denominator cannot equal 0.");
             return;
         }
-        if (num > 0 && denom < 0){
-            this.num = -num;
-            this.denom = Math.abs(denom);
+        if (num < 0 && denom < 0){
+            num = Math.abs(num);
+            denom = Math.abs(denom);
         }
-        else if (num < 0 && denom < 0){
-            this.num = Math.abs(num);
-            this.denom = Math.abs(num);
+        else if (denom < 0){
+            num = -num;
+            denom = Math.abs(denom);
+        }
+        else{
+            this.num = num;
+            this.denom = denom;
         }
     }
     public Fraction(int num){
@@ -47,37 +51,55 @@ public class Fraction
         return (double)(num / denom);
     }
     public Fraction add(Fraction other){
-        int addnum = num * other.getNumerator() + denom * other.getDenominator();
+        int addnum = num * other.getDenominator() + denom * other.getNumerator();
         int adddenom = denom * other.getDenominator();
         Fraction n = new Fraction(addnum, adddenom);
         n.toLowestTerms();
         return n;
     }
     public Fraction sub(Fraction other){
-        int subnum = num * other.getNumerator() - denom * other.getDenominator();
+        int subnum = num * other.getDenominator() + denom * other.getNumerator();
         int subdenom = denom * other.getDenominator();
         Fraction n = new Fraction(subnum, subdenom);
         n.toLowestTerms();
         return n;
     }
     public Fraction multi(Fraction other){
-        int multinum = n.getNumerator() * num;
-        int multidenom = n.getDenominator() * denom;
+        int multinum = other.getNumerator() * num;
+        int multidenom = other.getDenominator() * denom;
         Fraction n = new Fraction(multinum, multidenom);
         n.toLowestTerms();
         return n;
     }
     public Fraction div(Fraction other){
-        int
+        try{
+             int quotient = (num * other.getDenominator() / denom * other.getNumerator());
+        }
+        catch(IllegalArgumentException e){
+            System.out.println("Results in undefined, can't divide by 0!");
+        }
+        int divnum = num * other.getDenominator();
+        int divdenom = denom * other.getNumerator();
+        Fraction n = new Fraction(divnum, divdenom);
+        n.toLowestTerms();
+        return n;
+    }
+    public boolean equals(Object other){
+        Fraction k = (Fraction)other;
+        k.toLowestTerms();
+        if ((double)(num / denom) == (double)(k.getNumerator() / k.getDenominator())){
+            return true;
+        }
+        return false;
     }
     public void toLowestTerms(){
-        int gcd = new gcd(num, denom)
+        int gcd = gcd(num, denom);
         num  = num / gcd;
         denom = denom / gcd;
     }
     public static int gcd(int n, int d){
         int remainder;
-        while (d != 0 && a != 0){
+        while (d != 0 && n != 0){
             remainder = n % d;
             n = d;
             d = remainder;
