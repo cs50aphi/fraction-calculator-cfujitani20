2c33542993a0fc6538aae410a8d021320820db0e
cfujitani20 <cfujitani20@punahou.edu>
2020-04-26 08:24:02 +0000

    Finished most but divide and equals still need to work

--- Fraction.java
+++ Fraction.java
@@ -22,4 +22,66 @@ public class Fraction
             this.denom = Math.abs(num);
         }
     }
+    public Fraction(int num){
+        this.num = num;
+        denom = 1;
+    }
+    public Fraction(){
+        this.num = 0;
+        denom = 1;
+    }
+    public int getNumerator(){
+        return num;
+    }
+    public int getDenominator(){
+        return denom;
+    }
+    public String toString(){
+        if (denom == 1)
+        {
+            return Integer.toString(num);
+        }
+        return num + "/"+ denom;
+    }
+    public Double toDouble(){
+        return (double)(num / denom);
+    }
+    public Fraction add(Fraction other){
+        int addnum = num * other.getNumerator() + denom * other.getDenominator();
+        int adddenom = denom * other.getDenominator();
+        Fraction n = new Fraction(addnum, adddenom);
+        n.toLowestTerms();
+        return n;
+    }
+    public Fraction sub(Fraction other){
+        int subnum = num * other.getNumerator() - denom * other.getDenominator();
+        int subdenom = denom * other.getDenominator();
+        Fraction n = new Fraction(subnum, subdenom);
+        n.toLowestTerms();
+        return n;
+    }
+    public Fraction multi(Fraction other){
+        int multinum = n.getNumerator() * num;
+        int multidenom = n.getDenominator() * denom;
+        Fraction n = new Fraction(multinum, multidenom);
+        n.toLowestTerms();
+        return n;
+    }
+    public Fraction div(Fraction other){
+        int
+    }
+    public void toLowestTerms(){
+        int gcd = new gcd(num, denom)
+        num  = num / gcd;
+        denom = denom / gcd;
+    }
+    public static int gcd(int n, int d){
+        int remainder;
+        while (d != 0 && a != 0){
+            remainder = n % d;
+            n = d;
+            d = remainder;
+        }
+        return n;
+    }
 }
